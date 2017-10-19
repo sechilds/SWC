eb8094ddbf2db4408d4791fd7d813bf3e7a748e3
Stephen Childs <sechilds@gmail.com>
2017-10-19 13:27:27 -0400

    Initial commit

---
+++ gapminder_analysis.R
@@ -0,0 +1,65 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv", destfile = "data/gapminder-FiveYearData.csv")
+
+gapminder <- read.csv("data/gapminder-FiveYearData.csv")
+
+mean(gapminder$lifeExp)
+
+max(gapminder$year)
+gapminder$year == 2007
+
+gapminder2007 <- gapminder[gapminder$year == 2007,]
+mean(gapminder2007$lifeExp)
+mean(gapminder$lifeExp)
+
+plot(gapminder$lifeExp,gapminder$gdpPercap) 
+
+# start of ggplot2 section
+library(tidyverse)
+ggplot(data=gapminder,aes(x=gdpPercap,y=lifeExp)) +
+    geom_point()
+ggplot(data=gapminder,aes(x=year,y=lifeExp)) +
+    geom_point()
+
+
+ggplot(data=gapminder,aes(x=year,y=lifeExp,by=country,color=continent)) +
+    geom_point()+
+    geom_line()
+
+
+ggplot(data=gapminder,aes(x=gdpPercap,y=lifeExp, color=continent)) +
+    geom_point() +
+    scale_x_log10()+
+    geom_smooth(method="lm")
+
+
+
+ggplot(data=gapminder[gapminder$continent=="Americas",],aes(x=gdpPercap,y=lifeExp, color=continent, by=country)) +
+    geom_point() +
+    scale_x_log10()+
+    geom_smooth(method="lm")+
+    facet_wrap( ~ country)
+
+
+ggplot(data=gapminder[gapminder$continent=="Americas",],aes(x=gdpPercap,y=lifeExp, color=continent, by=country)) +
+    geom_point() +
+    scale_x_log10()+
+    geom_smooth(method="lm")+
+    facet_wrap( ~ country)
+
+# interactive plotting with plotly
+if(!require("plotly")){install.packages("plotly")}
+library(plotly)
+
+p <- ggplot(data=gapminder[gapminder$continent=="Americas",],aes(x=gdpPercap,y=lifeExp, color=continent, by=country)) +
+    geom_point() +
+    scale_x_log10()+
+    geom_smooth(method="lm")+
+    facet_wrap( ~ country)
+
+p
+ggplotly(p)
+
+# making nice labels
+p + labs(x="GDP per Capita", y="Life Expectancy (y)",
+         title="Figure 1") +
+    scale_color_discrete(name="Continent")
